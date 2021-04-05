341bf2d70c83a45df20e7ac354171d3884ef3028
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-04-05 15:27:38 -0400

    Building out notes functionality with components

---
+++ src/components/NoteSelector.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="note-selector" 
+  v-bind:class="{ active: selectedNoteId === note.id }" 
+  v-on:click="selectNote(note)">
+    <p class="note-selector-title">{{ note.body }}</p>
+    <p class="note-selector-timestamp">{{ note.timestamp }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["note", "selectedNoteId"],
+  methods: {
+    selectNote: function (note) {
+      this.$emit("selectNote", note);
+    },
+  },
+};
+</script>
---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="note-selectors">
+    <NoteSelector
+      v-for="note in notes"
+      v-bind:note="note"
+      v-bind:selectedNoteId="selectedNoteId"
+      v-on:selectNote="selectNote"
+      v-bind:key="note.id"
+    />
+    <!-- <NoteSelector v-for="note in notes" v-bind:note="note" v-bind:key="note.id" /> -->
+  </div>
+</template>
+
+<script>
+import NoteSelector from "./NoteSelector.vue";
+
+export default {
+  components: {
+    NoteSelector,
+  },
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "This is a first test", timestamp: Date.now() },
+        { id: 2, body: "This is a second test", timestamp: Date.now() },
+        { id: 3, body: "This is a third test", timestamp: Date.now() },
+        { id: 4, body: "This is a fourth test", timestamp: Date.now() },
+      ],
+      selectedNoteId: 1, //auto-select first item
+    };
+  },
+  methods: {
+    selectNote: function (note) {
+      this.selectedNoteId = note.id;
+    }
+  }
+};
+</script>
